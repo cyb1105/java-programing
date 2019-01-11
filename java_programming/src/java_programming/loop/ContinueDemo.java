e7167b5d79dd6c69591decbdc8a61d9a7a883e2b
RAN-CHAE-PC\Ran-chae <dbsqud1105@gmail.com>
2019-01-11 10:26:44 +0900

    no message

--- java_programming/src/java_programming/loop/ContinueDemo.java
+++ java_programming/src/java_programming/loop/ContinueDemo.java
@@ -3,7 +3,11 @@ package java_programming.loop;
 public class ContinueDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 for (int i = 0; i < 10; i++) {
+	            if (i == 5)
+	                continue;
+	            System.out.println("hi" + i);
+	        }
 
 	}
 
