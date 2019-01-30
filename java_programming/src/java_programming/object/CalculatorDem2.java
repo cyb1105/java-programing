a837b6150db806d9372f4723192a6feb5d4667b5
RAN-CHAE-PC\Ran-chae <dbsqud1105@gmail.com>
2019-01-30 18:39:39 +0900

    no message

---
+++ java_programming/src/java_programming/object/CalculatorDem2.java
@@ -0,0 +1,12 @@
+package java_programming.object;
+
+public class CalculatorDem2 {
+	 public static void sum(int left, int right) {
+	        System.out.println(left + right);
+	    }
+	public static void main(String[] args) {
+		 sum(10, 20);
+	     sum(20, 40);
+	}
+
+}
